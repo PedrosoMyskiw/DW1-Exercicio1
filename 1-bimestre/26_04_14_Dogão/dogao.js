f7fe8c64868bf348e4c525212ad80607027e59ea
PedrosoMyskiw <pedroso.myskiw@gmail.com>
2026-04-14 11:08:10 -0300

    DW1

---
+++ 1-bimestre/26_04_14_Dogão/dogao.js
@@ -0,0 +1,27 @@
+function calcular() {
+
+  let q1 = parseInt(document.getElementById("i1").value) || 0;
+  let q2 = parseInt(document.getElementById("i2").value) || 0;
+  let q3 = parseInt(document.getElementById("i3").value) || 0;
+  let q4 = parseInt(document.getElementById("i4").value) || 0;
+  let q5 = parseInt(document.getElementById("i5").value) || 0;
+
+  if(q1 < 0 || q2 < 0 || q3 < 0 || q4 < 0 || q5 < 0){
+    let resposta = document.getElementById("resposta");
+
+    resposta.innerText = "Digite valores válidos";
+    resposta.classList.add("erro");
+
+    setTimeout(() => {
+    resposta.classList.remove("erro");
+    }, 300);
+
+    return;
+  }
+
+  let total = (q1 * 22) + (q2 * 26) + (q3 * 29) + (q4 * 5) + (q5 * 8);
+  let tquantidade = q1 + q2 + q3 + q4 + q5;
+
+  document.getElementById("resposta").innerHTML = "Total: R$ " + total.toFixed(2).replace(".", ",") + "<br>Quantidade comprada: " + tquantidade;
+
+}
